b02eb8a35eb0211c32ad21886d69c979b569251a
Metztli <marquez@gmail.com>
2023-08-06 14:34:32 -0600

    Merge pull request #4 from SrChicharron/CH-15-Home-cliente-publicaciones Filtro para publicaciones

---
+++ src/screens/trabajador/Home-tipolimpieza/PubEspacioscomerciales.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PubEspacioscomerciales() {
+  return (
+    <View>
+      <Text>Espacio comercial</Text>
+    </View>
+  )
+}
